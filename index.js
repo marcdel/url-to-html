0bab38da7adc481b9c387a4683ad04e166ee9f17
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-11 11:41:23 -0800

    Make http request to get the html.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var http = require('http');
+
 exports.handler = function(event, context) {
-  context.succeed({url: event.url, bucket: event.bucket});
+  var options = {
+    host: event.host,
+    path: event.path
+  };
+
+  var req = http.request(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on('end', function () {
+      context.succeed({url: event.host + event.path, data: data, bucket: event.bucket});
+    });
+  });
+
+  req.on('error', function (e) {
+    context.fail({error: e.message})
+  });
+
+  req.end();
 }
